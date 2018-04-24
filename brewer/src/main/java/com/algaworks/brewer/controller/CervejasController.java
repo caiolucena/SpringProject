be8096690b368603d0faea1a6188d9214513a3ca
Caio Lucena <caiolucena.cc@gmail.com>
2018-04-23 23:32:56 -0300

    Testando método Get

--- brewer/src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ brewer/src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -3,12 +3,23 @@ package com.algaworks.brewer.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.algaworks.brewer.model.Cerveja;
 
 @Controller
 public class CervejasController {
 	
+	//SE for get chama esse
 	@RequestMapping("/cervejas/novo")
 	public String novo() {
+		return "cerveja/CadastroCerveja";
+	}
+	
+	//SE for post, chama esse
+	@RequestMapping(value = "/cervejas/novo",method = RequestMethod.POST)
+	public String  cadastrar(Cerveja cerveja) { //ela pega o nome lá do form
+		System.out.println("Cadastrar ="+cerveja.getSku());
 		return "cerveja/CadastroCerveja";
 	}
 }
