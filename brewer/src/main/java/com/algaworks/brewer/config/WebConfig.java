c0cb9abfbd2dd9fd103f202e9cc30521c424b6bf
Caio Lucena <caiolucena.cc@gmail.com>
2018-04-23 23:18:47 -0300

    Finalizando as configurações do Spring e do Inicializador

--- brewer/src/main/java/com/algaworks/brewer/config/WebConfig.java
+++ brewer/src/main/java/com/algaworks/brewer/config/WebConfig.java
@@ -1,9 +1,20 @@
 package com.algaworks.brewer.config;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 import com.algaworks.brewer.controller.CervejasController;
 
@@ -14,8 +25,46 @@ import com.algaworks.brewer.controller.CervejasController;
 @Configuration
 @ComponentScan(basePackageClasses = CervejasController.class)
 @EnableWebMvc
-public class WebConfig implements WebMvcConfigurer{
+public class WebConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware{
 	//usei o implements WebMvcConfigurer em vez
 	//do extends WebMvcConfigurerAdapter porque este tá deprecado
+	
+	private ApplicationContext applicationContext;
+	
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException{
+		this.applicationContext = applicationContext;
+	}
+	
+	
+	@Bean
+	public ViewResolver viewResolver() {
+		ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+		resolver.setTemplateEngine(templateEngine());
+		resolver.setCharacterEncoding("UTF-8");
+		return resolver;
+	}
+	
+	//Ele pega o templateResolver e o usa de fato
+	@Bean
+	public TemplateEngine templateEngine() {
+		SpringTemplateEngine engine = new SpringTemplateEngine();
+		engine.setEnableSpringELCompiler(true);
+		engine.setTemplateResolver(templateResolver());
+		return engine;
+	}
+	
+	//resolve os templates do html e diz onde estão
+	private ITemplateResolver templateResolver() {
+		SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+		resolver.setApplicationContext(applicationContext);
+		resolver.setPrefix("classpath:/templates/");
+		resolver.setCharacterEncoding("UTF-8");
+		resolver.setSuffix(".html");
+		resolver.setTemplateMode(TemplateMode.HTML);
+		return resolver;
+	}
+
+	
 	
 }
