079851017d22f59edf6bd97614182f9a490b37e3
Caio Lucena <caiolucena.cc@gmail.com>
2018-04-24 00:34:26 -0300

    Primeira view

--- brewer/src/main/java/com/algaworks/brewer/model/Cerveja.java
+++ brewer/src/main/java/com/algaworks/brewer/model/Cerveja.java
@@ -1,12 +1,20 @@
 package com.algaworks.brewer.model;
 
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotBlank;
+
 public class Cerveja {
 
+	@NotBlank
 	private String sku;
 	
+	@NotBlank
+	@Size(min=1)
 	private String nome;
 
-	
+	@Size(max=100)
+	private String descricao;
 	
 	public String getSku() {
 		return sku;
