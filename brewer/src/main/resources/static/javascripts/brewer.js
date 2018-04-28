5fd749c41949e25068f75b0760632e4407b1f5e4
Caio Lucena <caiolucena.cc@gmail.com>
2018-04-28 16:35:13 -0300

    Separando o Layout Padrão em Fragmentos

---
+++ brewer/src/main/resources/static/javascripts/brewer.js
@@ -0,0 +1,10 @@
+
+$(function(){
+	//Método pra formatar o input em numero decimal
+	var reais = $('.js-decimal');
+	reais.maskMoney({thousands:'.', decimal:','});
+
+	//Método pra formatar o input em numero inteiro
+	var plain = $('.js-plain');
+	plain.maskMoney({precision:0,thousands:"."});
+});
